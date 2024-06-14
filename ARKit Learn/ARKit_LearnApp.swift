b1974f2dee37e1b35ac63166c55185042641db49
knn3 <knn3@sfu.ca>
2024-06-13 19:57:31 -0700

    Initial Commit

---
+++ ARKit Learn/ARKit_LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKit_LearnApp.swift
+//  ARKit Learn
+//
+//  Created by Nguyên Nguyễn on 2024-06-13.
+//
+
+import SwiftUI
+
+@main
+struct ARKit_LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
